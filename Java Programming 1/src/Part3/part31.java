101bcb2de624b13d9a6c150105c725b88e731708
benjamin <benphang93@gmail.com>
2024-10-11 22:15:57 +0800

    Complete part 3-1

---
+++ Java Programming 1/src/Part3/part31.java
@@ -0,0 +1,28 @@
+package Part3;
+import java.util.Scanner;
+
+public class part31 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int values = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Provide a value, a negative value ends the program");
+            int value = Integer.parseInt(scanner.nextLine());
+            if (value < 0) {
+                break;
+            }
+
+            values = values + 1;
+            sum = sum + value;
+        }
+
+        if (sum == 0) {
+            System.out.println("The average of the values could not be calculated.");
+        } else {
+            System.out.println("Average of values: " + (1.0 * sum / values));
+        }
+    }
+
+}
